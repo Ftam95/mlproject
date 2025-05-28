664d9d55240ad08fe94939072feb34591f8dd05b
Tapiwamak <tapiwa.makamure@stewardbank.co.zw>
2025-05-28 12:11:53 +0200

    model training

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,7 @@
 import sys
+from src.logger import logging
+import os
+
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -14,6 +17,15 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
+
+
+### Check my logger
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         logging.info("Divide by zero error")
+#         raise CustomException(e, sys) from e
     
 
     
